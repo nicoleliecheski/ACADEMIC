a3729a00101d6194d79d1afbc7087688e2d2e16c
nicoleliecheski <131211908+nicoleliecheski@users.noreply.github.com>
2023-05-03 17:39:27 -0300

    Create PROBLEMA CHUVA.c
    Eventos climáticos extremos como chuvas descomunais estão cada vez mais frequentes e intensos em todo o mundo.
    
    O Centro Nacional de Monitoramento da Nlogônia tem medidores de quantidade de chuva dia-a-dia espalhados por todo o reino. Cada medição é um número inteiro, indicando a quantidade de chuva, em milímetros, que caiu na Nlogônia num determinado dia. Como o sistema existe há vários anos, a lista de medições é muito grande.
    
    Preocupado com o assunto, o rei da Nlogônia mandou que o Ministro da Ciência crie um programa de computador para calcular quantos intervalos de dias existem na lista de medições tal que a soma das medições nesse intervalo é igual a um certo valor.
    
    Mais precisamente, considere uma lista com N medições, indicando a quantidade de chuva do dia 1 ao dia N. Considere ainda todos os possíveis intervalos de dias entre 1 e N, cada intervalo definido pelo dia inicial e dia final do intervalo. O rei deseja saber quantos intervalos têm a soma das medições exatamente igual a um certo valor S.
    
    O Ministro da Ciência é um físico brilhante, mas não sabe resolver essa tarefa. Você poderia ajudá-lo?
    
    Input
    A primeira linha contém um inteiro N, o número de medições na lista. A segunda linha contém um inteiro S, o valor da soma desejada. A terceira linha contém N inteiros Xi , os valores das medições.
    
    Restrições:
    
    1 ≤ N ≤ 105.
    0 ≤ S ≤ 106.
    0 ≤ Xi ≤ 10, para 1 ≤ i ≤ N.
    
    Output
    Seu programa deve produzir uma única linha, contendo um único inteiro, que deve ser o número de intervalos que têm a soma das medições igual a S.
    
    NÃO FINALIZADO.

---
+++ TREINAMENTO MONKEYS/PROBLEMA CHUVA.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int N, S, X[N], r, i;
+    scanf("%d\n%d\n", &N, &S);
+    for (int c = 0; c < N; c++){
+        scanf("%d\n", X[c]);
+    }
+    
+    for (int c = 0; c < N; c++){
+        for (){
+            r = X[c] + X[c+c];
+            if (r == S){
+                i = i++;
+            }
+        }
+    }
+    
+    return 0;
+
+}
