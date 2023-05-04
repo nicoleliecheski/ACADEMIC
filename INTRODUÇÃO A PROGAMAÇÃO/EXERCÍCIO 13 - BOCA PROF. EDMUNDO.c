b728772ec90087698253042e3e302326b90e20b1
nicoleliecheski <131211908+nicoleliecheski@users.noreply.github.com>
2023-05-04 19:23:08 -0300

    Update EXERCÍCIO 13 - BOCA PROF. EDMUNDO.c
    Escrever um algoritmo que leia a massa (em toneladas) de um avião, sua aceleração (m/s²) e o tempo (s) que levou do repouso até a decolagem. O programa deve calcular e escrever a velocidade atingida (Km/h), o comprimento da pista (m) e o trabalho mecânico realizado (J) no momento da decolagem.

--- INTRODUÇÃO A PROGAMAÇÃO/EXERCÍCIO 13 - BOCA PROF. EDMUNDO.c
+++ INTRODUÇÃO A PROGAMAÇÃO/EXERCÍCIO 13 - BOCA PROF. EDMUNDO.c
@@ -5,7 +5,7 @@ int main() {
     //este programa foi feito pela aluna: Nicole Liecheski
     //na data 02/05/2023
 
-    //Escreva o valor do preço de fábrica do carro, do percentual do distribuidor e do percentual de impostos.
+    //Escreva o valor a massa (em toneladas) de um avião, sua aceleração (m/s²) e o tempo (s) que levou do repouso até a decolagem
 
     scanf("\n%lf\n%lf\n%lf", &massa, &ac, &seg);
     printf("VELOCIDADE = %.2lf\nESPACO PERCORRIDO = %.2lf\nTRABALHO REALIZADO = %.2lf\n", ac*seg*3.6, (ac*seg*seg)/2, ((massa*1000)*(ac*seg*ac*seg))/2);
