58706c9afc1d2945c0113d31d5dcc7affa33b67f
nicoleliecheski <131211908+nicoleliecheski@users.noreply.github.com>
2023-05-08 20:50:08 -0300

    Update EXERCÍCIO 22 - BOCA PROF. EDMUNDO.c
    Desenvolver um programa para calcular a conta de água para uma empresa de saneamento. O custo da água varia dependendo se o consumidor é residencial, comercial ou industrial. A regra para calcular a conta é:
    
    • Residencial: R$ 5,00 de taxa mais R$ 0,05 por metros cúbicos gastos;
    • Comercial: R$ 500,00 para os primeiros 80 metros cúbicos gastos mais R$ 0,25 por metros cúbicos excedentes;
    • Industrial: R$ 800,00 para os primeiros 100 metros cúbicos gastos mais R$ 0,04 por metros cúbicos excedentes;
    
    O programa deverá ler a conta do cliente, o consumo de água por metros cúbicos e o tipo de consumidor (residencial, comercial e industrial). Como resultado, o programa deve imprimir a conta do cliente e o valor em reais a ser pago pelo mesmo.

--- INTRODUÇÃO A PROGAMAÇÃO/EXERCÍCIO 22 - BOCA PROF. EDMUNDO.c
+++ INTRODUÇÃO A PROGAMAÇÃO/EXERCÍCIO 22 - BOCA PROF. EDMUNDO.c
@@ -5,26 +5,26 @@ int main() {
     float consumo, valor;
     char tipo; //declaração das variáveis
     //este programa foi feito pela aluna: Nicole Liecheski
-    //na data 04/05/2023
+    //na data 08/05/2023
 
-   //Escreva três números inteiros de um dígito.
+   //Escreva a conta do cliente (um número inteiro), o consumo de água por metros cúbicos (float) e o tipo do consumidor (um caractere: ‘C’ - COMERCIAL, ‘I’ - INDUSTRIAL ou ‘R’ - RESIDENCIAL).
 
     scanf("%d %f %c", &conta, &consumo, &tipo);
-    if (tipo == "R") {
+    if (tipo == 'R') {
         valor = consumo*0.05+5;
     }
-    if (tipo == "C") {
-        if (consumo <= 80){
-            valor = 500.0;
+    if (tipo == 'C') {
+        if (consumo <= 80.00){
+            valor = 500.00;
         } else {
-            valor = (consumo-80)*0.25+500.0;
+            valor = (consumo-80.00)*0.25+500.00;
         }
     }
-    if (tipo == "I") {
-        if (consumo <= 100){
-            valor = 800.0;
+    if (tipo == 'I') {
+        if (consumo <= 100.00){
+            valor = 800.00;
         } else {
-            valor = (consumo-80)*0.04+800.0;
+            valor = (consumo-100.00)*0.04+800.00;
         }
     }
     printf("CONTA = %d\nVALOR DA CONTA = %.2f\n", conta, valor);
