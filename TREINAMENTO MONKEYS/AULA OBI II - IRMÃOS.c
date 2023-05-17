98a23b2c0707010f7c8292f4840cc34ce185f9f8
nicoleliecheski <131211908+nicoleliecheski@users.noreply.github.com>
2023-05-17 14:44:30 -0300

    Create AULA OBI II - IRMÃOS.c
    Otávio tem dois irmãos, um mais velho (Orlando) e um mais novo do que ele (Oscar). As idades dos três irmãos formam uma progressão aritmética: a diferença de idade dos dois irmãos mais novos (Otávio e Oscar) é igual à diferença de idade dos dois irmãos mais velhos (Orlando e Otávio).
    
    Dadas as idades de Otávio e de seu irmão mais novo, escreva um programa para determinar a idade do irmão mais velho.
    
    Input
    A primeira linha da entrada contém um inteiro N, a idade do irmão mais novo de Otávio. A segunda linha contém um inteiro M, a idade de Otávio.
    
    1≤N≤40
    N≤M≤40
    
    Output
    Seu programa deve produzir na saída uma única linha, contendo um único número inteiro, a idade do irmão mais velho de Otávio.

---
+++ TREINAMENTO MONKEYS/AULA OBI II - IRMÃOS.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(){
+
+    int res, g;
+
+    for (int i = 0; i < 12; i++){
+        char R;
+        scanf("%c", &R);
+        if (R == 'V'){
+            res++;
+        }
+        //printf("%d\n", i);
+    }
+
+    if (res>4){
+        g = 1;}
+    else {
+        if (res>2){
+            g = 2;}
+        else {
+            if (res == 0){
+                g = -1;}
+            else{
+                g = 3;
+            } 
+        }
+    }
+    
+
+    printf("%d\n", g);
+
+    return 0;
+}
