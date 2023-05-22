a1f0b05ae3d47e1795ef9c8848c2f943e590048f
nicoleliecheski <131211908+nicoleliecheski@users.noreply.github.com>
2023-05-21 22:19:58 -0300

    Update EXERCÍCIO 03 - BOCA PROF. EDMUNDO.c

--- INTRODUÇÃO A PROGRAMAÇÃO/EXERCÍCIOS BOCA/EXERCÍCIO 03 - BOCA PROF. EDMUNDO.c
+++ INTRODUÇÃO A PROGRAMAÇÃO/EXERCÍCIOS BOCA/EXERCÍCIO 03 - BOCA PROF. EDMUNDO.c
@@ -3,7 +3,7 @@
 int main() {
     float temp, chuva; //declaração das variáveis
     //este programa foi feito pela aluna: Nicole Liecheski
-    //na data 27/04/2022
+    //na data 27/04/2023
 
     //Escreva o valor da temperatura e da quantidade de chuva;
 
