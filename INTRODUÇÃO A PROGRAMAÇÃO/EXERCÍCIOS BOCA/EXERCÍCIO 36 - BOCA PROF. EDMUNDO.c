f64e20a24cc3001bfeaa3307cc6b5b9f831f202b
nicoleliecheski <131211908+nicoleliecheski@users.noreply.github.com>
2023-05-22 00:39:23 -0300

    Update EXERCÍCIO 36 - BOCA PROF. EDMUNDO.c

--- INTRODUÇÃO A PROGRAMAÇÃO/EXERCÍCIOS BOCA/EXERCÍCIO 36 - BOCA PROF. EDMUNDO.c
+++ INTRODUÇÃO A PROGRAMAÇÃO/EXERCÍCIOS BOCA/EXERCÍCIO 36 - BOCA PROF. EDMUNDO.c
@@ -1,29 +1,51 @@
 #include <stdio.h>
 
 int main() {
-    int num; //declaração das variáveis
+    int num, alg[5]; //declaração das variáveis
     //este programa foi feito pela aluna: Nicole Liecheski
-    //na data 16/05/2023
+    //na data 22/05/2023
 
-   //Escreva um número inteiro.
+   //Escreva um número inteiro de até 5 digítos.
 
     scanf("%d", &num);
+    alg[0] = (num - num%10000)/10000;
+    alg[1] = (num%10000 - num%1000)/1000;
+    alg[2] = (num%1000 - num%100)/100;
+    alg[3] = (num%100 - num%10)/10;
+    alg[4] = num%10;
+    
     if (num>99999){
-        printf("NUMERO INVALIDO");}
+        printf("NUMERO INVALIDO\n");}
     else {
         if(num>9999){
-            }
+            if((alg[0]==alg[4])&&(alg[1]==alg[3])){
+                printf("PALINDROMO\n");}
+            else{
+                printf("NAO PALINDROMO\n");}
+        }
         else{
             if(num>999){
+                if((alg[1]==alg[4])&&(alg[2]==alg[3])){
+                    printf("PALINDROMO\n");}
+                else{
+                    printf("NAO PALINDROMO\n");}
             }
             else{
                 if(num>99){
+                    if(alg[2]==alg[4]){
+                        printf("PALINDROMO\n");}
+                    else{
+                        printf("NAO PALINDROMO\n");}
                 }
                 else{
                     if(num>9){
+                        if(alg[3]==alg[4]){
+                            printf("PALINDROMO\n");}
+                        else{
+                            printf("NAO PALINDROMO\n");}
                     }
                     else{
-                        
+                        printf("PALINDROMO\n");
                     }
                 }
             }
