a9371c6ceb831ff6fc351ec971320a14829e8491
nicoleliecheski <131211908+nicoleliecheski@users.noreply.github.com>
2023-05-24 01:51:40 -0300

    Create EXERCÍCIO 43 - BOCA PROF. EDMUNDO.c
    Não finalizado.

---
+++ INTRODUÇÃO A PROGRAMAÇÃO/EXERCÍCIOS BOCA/EXERCÍCIO 43 - BOCA PROF. EDMUNDO.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+
+    int V[1000], N, i, res; //declaração das variáveis
+    //este programa foi feito pela aluna: Nicole Liecheski
+    //na data 24/05/2023
+
+    //Faça um programa que receba um vetor V de N inteiros e imprima os elementos pares do vetor V.
+    //A entrada contém apenas um caso de teste com 2 linhas. Na primeira linha há um inteiro N,
+    //1 < N ≤ 1000, representando o tamanho do vetor V. Na segunda linha há N valores inteiros 
+    //separados por um espaço em branco cada, que são os valores do vetor V.
+
+
+    scanf("%d\n", &N);
+
+    for(i = 0; i < N; ++i){
+        V[i] = 0;
+    }
+
+    for(i = 0; i < N; ++i){
+        scanf("%d ", &V[i]);
+    }
+
+    res = 0;
+    for(i = 0; i < N; ++i){
+        if ((V[i]%2)==0) {
+            res++;
+            printf("%d ", V[i]);
+        }
+    }
+
+    printf("%d\n", res);
+    return 0;
+}
