3368205ada3e6f71fd23e10a26aa872387bc47f1
Nicole Liecheski <131211908+nicoleliecheski@users.noreply.github.com>
2023-11-30 21:53:52 -0300

    Create EXERCÍCIO 4.c

---
+++ ALGORITMOS E ESTRUTURAS DE DADOS I/PROVA I/EXERCÍCIO 4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int c = 0;
+char P[260][]
+
+void perm(char S[260]){ // função para implementar a recursividade
+    
+}
+
+int main (){
+    int i;
+    char S[260];
+
+    memset(M, -1, sizeof(M)); // definindo todos os espaços da matriz como -1 (serve para verificar se aquela posição já foi preenchida)
+
+    fgets(S, 260, stdin); //  entrada
+
+    perm(S); // chamo a a funçao pela primeira vez
+
+    for (i = 0; i < c; i++) // impressao dos anagramas
+    {
+        
+    }   
+
+    return 0;
+}
