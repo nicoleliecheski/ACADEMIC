4534e26b4215bbb2edbc1587dd9fda7799e0e24c
Nicole Liecheski <131211908+nicoleliecheski@users.noreply.github.com>
2024-05-03 14:32:36 -0300

    Create B.cpp

---
+++ TÓPICOS AVANÇADOS DE PROGRAMAÇÃO V/TREINO 04/B.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 2*1e5 + 500;
+
+typedef long long ll;
+
+ll soma[N];
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n, q;
+
+    cin >> n >> q;
+
+    for (ll i = 1; i <= n; i++)
+    {
+        ll a;
+        cin >> a;
+        soma[i] = soma[i-1] ^ a;
+        //cout << "soma " << i << ": " << soma[i] << "\n";
+    }
+    
+    for (ll i = 0; i < q; i++)
+    {
+        ll a, b, r;
+        cin >> a >> b;
+        r = soma[b] ^ soma[a-1];
+        cout << r << "\n";
+    }
+    
+    
+    return 0;
+}
