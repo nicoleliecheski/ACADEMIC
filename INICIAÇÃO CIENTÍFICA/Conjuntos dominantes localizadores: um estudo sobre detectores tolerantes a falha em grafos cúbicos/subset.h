88e70bf9f68e637f00d6342bcde988263ebaefda
Nicole Liecheski <131211908+nicoleliecheski@users.noreply.github.com>
2026-02-27 10:01:34 -0300

    Create subset.h for function declarations
    Add custom header for subset.cpp functions usage

---
+++ INICIAÇÃO CIENTÍFICA/Conjuntos dominantes localizadores: um estudo sobre detectores tolerantes a falha em grafos cúbicos/subset.h
@@ -0,0 +1,11 @@
+// Cabecalho personalizado para que as funcoes do arquivo subset.cpp possam ser utilizadas em outros arquivos
+
+#include <set>
+using namespace std;
+
+#ifndef SUBSET_H
+#define SUBSET_H
+
+set< set<int> > subsets(int n);
+
+#endif // SUBSET_H
