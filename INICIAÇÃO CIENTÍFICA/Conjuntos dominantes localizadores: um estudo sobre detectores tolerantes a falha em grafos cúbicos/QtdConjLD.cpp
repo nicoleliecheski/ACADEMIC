132e9eb1ad04a0d834dd296fa9549cafef60528f
Nicole Liecheski <131211908+nicoleliecheski@users.noreply.github.com>
2026-02-27 11:55:21 -0300

    Fix compilation command in QtdConjLD.cpp
    Updated compilation command in comments to reference graphio.c instead of graphio.o.

--- INICIAÇÃO CIENTÍFICA/Conjuntos dominantes localizadores: um estudo sobre detectores tolerantes a falha em grafos cúbicos/QtdConjLD.cpp
+++ INICIAÇÃO CIENTÍFICA/Conjuntos dominantes localizadores: um estudo sobre detectores tolerantes a falha em grafos cúbicos/QtdConjLD.cpp
@@ -1,6 +1,6 @@
 // Programa que calcula a quantidade de conjuntos LD (Localizadores Dominantes) possiveis de um grafo G qualquer
 // Utiliza funcoes dos cabecalhos personalizados "subset.h" e "showg.h"
-// Para compilar utilize o comando "g++ -o QtdConjLD QtdConjLD.cpp subset.cpp graphio.o"
+// Para compilar utilize o comando "g++ -o QtdConjLD QtdConjLD.cpp subset.cpp graphio.c"
 
 #include <iostream>
 #include <vector>
