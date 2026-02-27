acf2641a6077fa8bcd0e6fe0ee22fd1c0a30fb67
Nicole Liecheski <131211908+nicoleliecheski@users.noreply.github.com>
2026-02-27 12:18:56 -0300

    Refactor RED-OLD graph processing and comments

---
+++ INICIAÇÃO CIENTÍFICA/Conjuntos dominantes localizadores: um estudo sobre detectores tolerantes a falha em grafos cúbicos/Conjuntos_RED_OLD.cpp
@@ -0,0 +1,178 @@
+// Programa que calcula a quantidade de conjuntos RED-OLD (Redudantes Abertos Localizadores Dominantes) possiveis de varios grafos G quaisquer
+// Os grafos estao armazenados em arquivos do tipo .g6
+// Utiliza funcoes dos cabecalhos personalizados "subset.h" e "graphio.h"
+// Para compilar utilize o comando "g++ -o Conjuntos_RED_OLD Conjuntos_RED_OLD.cpp subset.cpp graphio.c"
+// Para executar utilize o comando "./Conjuntos_RED_OLD <nome do arquivo>.g6"
+
+#include "graphio.h"
+#include "subset.h"
+#include <iostream>
+#include <set>
+#include <stdio.h>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+// Funcao que retorna um set com os vizinhos de um vertice v
+set<int> vizinhos(unsigned long *g, int v, int m) { 
+  int j1, j2;
+  unsigned long *set1;
+  set1 = g + v * m;
+  set<int> viz;
+
+  j1 = -1;
+  while ((j1 = nextelement((unsigned long *)set1, m, j1)) >= 0) {
+    viz.insert(j1);
+  }
+
+  return viz;
+}
+
+int main(int argc, char *argv[]) {
+  // Abrindo o arquivo de entrada do tipo .g6
+  // Um arquivo so pode conter varios grafos mas todos possuem o mesmo tamanho de V(G) 
+  if (argc < 2) {
+    fprintf(stderr, "Usage: %s <input.g6>\n", argv[0]);
+    return -1;
+  }
+  FILE *f = fopen(argv[1], "r");
+  if (f == NULL) {
+    fprintf(stderr, "Error: Unable to open file '%s'\n", argv[1]);
+    return -1;
+  }
+
+  // Quantidade de grafos do arquivo de entrada que possuem um conjunto RED-OLD possivel
+  int Qtd_Grafos_com_RED_OLD = 0;
+
+  // Valores (tamanhos) possiveis dos conjuntos RED-OLD dos grafos de entrada
+  set<int> Vlrs_Possiveis_RED_OLD;
+
+  // Tamanho de V(G)
+  int n = 0;
+
+  // Verificacao para saber se os grafos do arquivo possuem cintura maior ou igual a 5
+  int g5 = 0;
+  cout << "g(G) >= 5? (1/0)\n";
+  cin >> g5;
+
+  char *s;
+  int graph_num = 0;
+  // Loop para pegar todos os grafos no arquivo .g6
+  while ((s = showg_getline(f)) != NULL) { 
+    // Quantidade de conjuntos RED-OLD que o grafo atual possui
+    int Qtd_RED_OLDs = 0;
+    // Indice do grafo atual
+    graph_num++;
+
+    // Otimizando: o n eh sempre igual nos arquivos
+    if(n == 0) n = graphsize(s); 
+
+    // Gerando todos os pares de vertices distintos possiveis do grafo
+    vector<pair<int, int> > pares_distintos;
+    for (int i = 0; i < n; i++) {
+      for (int j = i + 1; j < n; j++) {
+        pair<int, int> par(i, j);
+        pares_distintos.push_back(par);
+      }
+    }
+
+    // Tratamento dos dados do arquivo para poderem ser manipulados
+    int m = graph_row_words(n);
+    vector<unsigned long> g((size_t)n * m);
+    stringtograph(s, g.data(), m);
+
+    // Gerando a vizinhanca de cada vertice
+    vector<set<int> > vizinhancas;
+    for (int i = 0; i < n; i++) {
+      set<int> vizi = vizinhos(g.data(), i, m);
+      vizinhancas.push_back(vizi);
+    }
+    
+    // Todos os subconjuntos S possiveis de vertices desse grafo
+    set<set<int> > conjuntos_S = subsets(n); 
+
+    // Loop passando por cada conjunto S
+    for (auto &S : conjuntos_S) {        
+      
+      // Otimizando: se o conjunto atual eh vazio passa pro proximo conjunto
+      if(S.empty()) continue; 
+
+      // Otimizando: os limites de RED-OLD(G) sao (2/3)*n <= RED-OLD(G) <= n
+      if(((2/3) * n) > S.size()) continue; 
+
+      // Otimizando: se o grafo possui cintura[g(G)] >= 5, entao o conjunto V(G) eh RED-OLD
+      if(g5 && S.size() == n){ 
+        Qtd_RED_OLDs++;
+        Vlrs_Possiveis_RED_OLD.insert(S.size());
+        continue;
+      }  
+
+      // Flag pra indicar se o conjunto S atual eh RED-OLD ou nao
+      int eh_RED_OLD = 1; 
+
+      // Loop passando por cada par de vertice distinto u e v do grafo
+        for (auto &pp : pares_distintos) { 
+            int flag_u = 0;
+            int flag_v = 0;
+
+            // Loop passando por cada elemento do conjunto S atual
+            for (auto &s : S) { 
+
+                auto tem_u = vizinhancas[s].find(pp.first);
+                auto tem_v = vizinhancas[s].find(pp.second);
+                auto s_end = vizinhancas[s].end();
+
+                // Conferindo se a vizinhanca do elemento de S atual tem APENAS o u
+                if(tem_u != s_end && tem_v == s_end) { 
+                    flag_u = 1;
+                    continue;
+                } 
+                // Conferindo se a vizinhanca do elemento de S atual tem APENAS o v
+                else if(tem_v != s_end && tem_u == s_end) { 
+                    flag_v = 1;
+                    continue;
+                } 
+            }
+
+            // Se alguma dessas flag ainda eh false entao o conjunto S atual nao cobriu esse par logo S nao eh RED-OLD
+            if(!flag_u || !flag_v){ 
+                eh_RED_OLD = 0;
+                break;
+            }
+        }
+
+        // Se essa flag ainda eh true entao esse conjunto S eh RED-OLD
+        if(eh_RED_OLD){ 
+            Qtd_RED_OLDs++;
+
+            // Guardando o tamanho de S como um valor possivel de RED-OLD(G)
+            Vlrs_Possiveis_RED_OLD.insert(S.size()); 
+        }
+    }
+
+    // Se algum conjunto S eh RED-OLD entao significa que esse grafo tem um conjunto RED-OLD possivel
+    if(Qtd_RED_OLDs > 0){ 
+        Qtd_Grafos_com_RED_OLD++;
+    }
+  }
+
+  // Verificando se o arquivo nao esta vazio
+  if (graph_num == 0) {
+    fprintf(stderr, "Error: No graph line read (empty file?)\n");
+    fclose(f);
+    return -1;
+  } else {
+    cout << "Quantidade de grafos = " << graph_num << endl;
+    cout << "Quantidade de grafos com RED-OLD = " << Qtd_Grafos_com_RED_OLD << endl;
+
+    cout << "Valores possiveis para RED-OLD(G) = ";
+    for(auto &r : Vlrs_Possiveis_RED_OLD){
+      cout << r << " ";
+    }
+    cout << "\n";
+  }
+
+  fclose(f);
+  return 0;
+}
