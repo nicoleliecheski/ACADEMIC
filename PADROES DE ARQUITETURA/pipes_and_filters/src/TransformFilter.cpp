91b875b59f58c0e33fdc9d7e73d4f5efe94fda04
Nicole Liecheski <nicoleliecheski.pr@gmail.com>
2026-08-28 22:37:38 -0300

    tirando o std::

--- PADROES DE ARQUITETURA/pipes_and_filters/src/TransformFilter.cpp
+++ PADROES DE ARQUITETURA/pipes_and_filters/src/TransformFilter.cpp
@@ -1,7 +1,9 @@
 #include "TransformFilter.h"
 
-std::vector<TransformedRecord> TransformFilter::process(const std::vector<SaleRecord>& input) const {
-    std::vector<TransformedRecord> output;
+using namespace std;
+
+vector<TransformedRecord> TransformFilter::process(const vector<SaleRecord>& input) const {
+    vector<TransformedRecord> output;
 
     for (const auto& sale : input) {
         TransformedRecord transformed;
